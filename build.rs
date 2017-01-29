3d32f97f8827c420f63e591a817a4a71790e4499
David Eichmann <EichmannD@gmail.com>
2017-01-29 21:56:22 +0000

    Disable building chipmunk demo and shared library.

--- build.rs
+++ build.rs
@@ -3,8 +3,12 @@ extern crate cmake;
 fn main() {
     // Builds the project in the directory located in `chipmunk`, installing it
     // into $OUT_DIR
-    let dst = cmake::build("chipmunk");
+    let dst = cmake::Config::new("chipmunk")
+            .define("BUILD_DEMOS", "OFF")
+            .define("INSTALL_DEMOS", "OFF")
+            .define("BUILD_SHARED", "OFF")
+            .build();
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=chipmunk");
 }
